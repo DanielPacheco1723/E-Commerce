9cee034ac666c415eabc050de6ffdfe260e03bc9
Daniel Pacheco <danielfdelgado.dg@gmail.com>
2022-08-28 18:32:11 -0500

    Texto Enriquecido en Admin
    Adición de Tinymce para los campos de texto en el panel admin

--- src/MercadoCerrado/MercadoCerrado/settings.py
+++ src/MercadoCerrado/MercadoCerrado/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalogo.apps.CatalogoConfig',
-    'djmoney',
+    'tinymce',
+    'djmoney'
 ]
 
 MIDDLEWARE = [
--- src/MercadoCerrado/catalogo/models.py
+++ src/MercadoCerrado/catalogo/models.py
@@ -1,12 +1,13 @@
 from distutils.command.upload import upload
 from djmoney.models.fields import MoneyField
+from tinymce import models as tinymce_models
 from django.db import models
 
 
 class Producto (models.Model):
     nombre = models.CharField ( max_length = 60)
     precio = MoneyField ( default = 0, default_currency = 'MXN', max_digits = 11)
-    descripcion = models.TextField ( max_length = 200)
+    descripcion = tinymce_models.HTMLField ()
     imagen = models.ImageField ( upload_to = "productos", null = True)
     agregado = models.DateTimeField (auto_now_add = True)
 
